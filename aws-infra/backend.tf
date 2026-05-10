9b0c2e24af9da15505c9833477128d4f2700df4e
Adeife79 <ifeoluwaaderoju6@gmail.com>
2026-05-10 11:54:27 -0700

    initial commit

--- aws-infra/backend.tf
+++ aws-infra/backend.tf
@@ -1,7 +1,7 @@
-#terraform {
-#    backend "s3" {
-#        bucket = "todo-app-tf-state-bucket"
-#        key = "terraform.tfstate"
-#        region = "us-east-1"
-#    }
-#}
+terraform {
+    backend "s3" {
+        bucket = "todo-app-tf-state-bucket"
+        key = "terraform.tfstate"
+        region = "us-east-1"
+    }
+}
