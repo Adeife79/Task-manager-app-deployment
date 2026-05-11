0a99e3dd7ff8422fe6e331f69599b53eba74d3ce
Adeife79 <ifeoluwaaderoju6@gmail.com>
2026-05-11 05:02:09 -0700

    initial commit

--- aws-infra/security.tf
+++ aws-infra/security.tf
@@ -27,6 +27,14 @@ resource "aws_security_group" "sg" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
+    ingress {
+        description = "Backend Port"
+        from_port = var.backend_port
+        to_port = var.backend_port
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
     egress {
         from_port = 0
         to_port = 0
